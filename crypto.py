614287bbd4548b962bb6c97e3b67b76919fffc53
Michal Kot <michal.kot@burberry.com>
2013-10-26 21:51:12 +0100

    handle dropbox uploads, improvements

--- crypto.py
+++ crypto.py
@@ -2,13 +2,14 @@
 '''Crypto tastic
 
 Usage:
-  crypto.py en FILE [-o FILE]
-  crypto.py de FILE [-o FILE]
+  crypto.py en FILE [-o FILE] [--dropbox]
+  crypto.py de FILE [-o FILE] [--dropbox]
+  crypto.py de [-o FILE] --dropbox
   crypto.py (-h | --help)
   crypto.py --version
 
 Options:
-  -o FILE       Specify output file [default: ./test.txt]
+  -o FILE       Specify output file [default: ./out.crypto]
   -h --help     Show this screen.
   --version     Show version.
 
@@ -18,14 +19,15 @@ from docopt import docopt
 from Crypto.Cipher import AES
 from Crypto import Random
 from getpass import getpass
+from dropbox import client, rest
 
 BS = AES.block_size
 
 class C:
     HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
+    OKB = '\033[94m'
+    OKG = '\033[92m'
+    WARN = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
@@ -39,8 +41,11 @@ def read_in_chunks(file_object, chunk_size=1024):
 		else:
 			break
 
+def LOG(msg, COLOR=C.OKB):
+	print COLOR + msg + C.ENDC
+
 def fail(msg):
-	print C.FAIL + msg + C.ENDC
+	LOG(msg, C.FAIL)
 	sys.exit(1)
 
 def fopen(name, perm):
@@ -63,8 +68,60 @@ unpad = lambda s : s[0:-ord(s[-1])]
 def hash(msg):
 	return hashlib.new('ripemd160', msg).digest()
 
+TOKEN_FILE = "token_store.txt"
+token = 'RCzUIPNfK-0AAAAAAAAAASL3XaO6aQryvrJ9CECVHbN1d7E2lerM8sObRF8X5Dmm'
+BIG_FILE_TRESHOLD = 400000
+def uploadToDropbox(encrypted, fname, override=True):
+	fsize = os.path.getsize(encrypted)
+	fileHandle = open(encrypted, 'rb')
+	api_client = client.DropboxClient(token)
+	if (fsize > BIG_FILE_TRESHOLD):
+		chunkedUploadDropbox(fileHandle, fname, fsize, api_client)
+		return
+	# token = open(TOKEN_FILE).read()
+	response = api_client.put_file("/" + fname + ".crypto", fileHandle, override)
+	print response
+
+def size_fmt(num):
+    for x in ['bytes','kB','MB','GB','TB']:
+        if num < 1000:
+            return "{0:3.1f}{1}".format(num, x)
+        num /= 1000.0
+
+def chunkedUploadDropbox(handle, fname, size, client):
+	offset = 0
+	retry_attempt = 0
+	length = 100000
+	LOG('Uploading {0} in {1} chunks'.format(size_fmt(size), size_fmt(length)))
+	while offset < size:
+		try:
+			client.upload_chunk(handle, length, offset)
+			LOG('{:.1%}'.format(offset / float(size)), '')
+			offset += length
+		except rest.ErrorResponse as e:
+			print e
+			retry_attempt += 1
+			LOG('Error; retry #' + str(retry_attempt), C.WARN)
+			pass
+		except rest.RESTSocketError:
+			fail('Could not connect to dropbox.com')
+	LOG('SUCCESS', C.OKG)
+
+def getDropboxFile(fname):
+	handle = open("." + fname + ".tmp", "wb")
+	api_client = client.DropboxClient(token)
+	f = api_client.get_file("/" + fname + ".crypto")
+	handle.write(f.read())
+	handle.close()
+	return "." + fname + ".tmp"
+
 def crypto(args):
-	f = fopen(args['FILE'], 'rb')
+	fname = args['FILE']
+	outfile = args['-o']	
+	if args['de'] and args['--dropbox']:
+		outfile = fname
+		fname = getDropboxFile(fname)
+	f = fopen(fname, 'rb')
 	pass1 = getpass('Enter your password: ')
 	if args['en']:
 		size = os.path.getsize(args['FILE'])
@@ -73,6 +130,7 @@ def crypto(args):
 			key = pad(pass1)
 		else:
 			fail('Password does not match')
+		LOG('ENCRYPTION...')
 		message = b''
 		iv = Random.new().read(BS)
 		for piece in read_in_chunks(f, 16):
@@ -85,10 +143,15 @@ def crypto(args):
 			'message': message,
 			'hash': hash(pass1)
 		}
-		o = fopen(args['-o'], 'wb')
+		o = fopen(outfile, 'wb')
 		h = hash(pass1)
 		pack = iv + h + message
+		LOG('SUCCESS', C.OKG)
 		o.write(pack)
+		o.close()
+		if args['--dropbox']:
+			uploadToDropbox(outfile, fname)
+			os.remove(outfile)
 	elif args['de']:
 		data = f.read(BS+20)
 		iv = data[:BS]
@@ -98,11 +161,13 @@ def crypto(args):
 			message = b''
 			for piece in read_in_chunks(f, 16):
 				message += decrypt(iv, key, piece)
-			o = fopen(args['-o'], 'wb')
+			o = fopen(outfile, 'wb')
 			o.write(unpad(message))
+			if args['--dropbox']:
+				os.remove(fname)
 		else:
 			fail('Incorrect password.')
 
 if __name__ == '__main__':
-	args = docopt(__doc__, version='Crypto 0.0.1')
+	args = docopt(__doc__, version='0.0.1')
 	crypto(args)
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 time python crypto.py en crypto.py;
-time python crypto.py de test.txt -o out.tst;
+time python crypto.py de out.crypto -o out.tst;
 md5 crypto.py out.tst;
-rm test.txt;
+rm out.crypto;
 rm out.tst;
